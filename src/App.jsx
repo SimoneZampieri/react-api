698753ec9620ec9fab4632409fe10572fbe3d4fe
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-22 15:18:17 +0100

    [update] app.jsx now has state functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import Posts from ('./components/Posts')
+import { useState, useEffect } from "react";
+import axios from ('axios')
 
 function App() {
+
+  const initialFormData = {
+    id: "",
+    titolo: "",
+    didascalia: "",
+    categoria: "",
+    immagini: "",
+    tags: [],
+    stato: "",
+  };
+
+  const baseApiUrl = import.meta.VITE_BASE_API_URL;
+  const [posts, setPosts] = useState([])
+  const [formData, setFormData] = useState(initialFormData)
+  
   return (
     <>
-      <Posts />
     </>
   );
 }
