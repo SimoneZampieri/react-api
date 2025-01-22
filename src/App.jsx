b135f0e5e55c1f519768d581a42e1d1e3ff2eb58
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-22 15:27:32 +0100

    [update] added fetch

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,20 @@ function App() {
     id: "",
     titolo: "",
     didascalia: "",
-    categoria: "",
     immagini: "",
-    tags: [],
-    stato: "",
+    tags: []
   };
 
   const baseApiUrl = import.meta.VITE_BASE_API_URL;
   const [posts, setPosts] = useState([])
   const [formData, setFormData] = useState(initialFormData)
-  
+
+  const fetchPosts = () => (
+    axios.get(`${baseApiUrl}/posts`)
+    .then(res => (
+      setPosts(res.data)
+    ))
+  )
   return (
     <>
     </>
