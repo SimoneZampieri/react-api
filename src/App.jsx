ffc3b477a559bc96daa66af97ec565c510c76cdd
SimoneZampieri <simonezampieri106@gmail.com>
2025-01-22 15:54:40 +0100

    [update] app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,28 @@
 import { useState, useEffect } from "react";
-import axios from ('axios')
+import axios from "axios";
 
 function App() {
-
   const initialFormData = {
     id: "",
     titolo: "",
     didascalia: "",
     immagini: "",
-    tags: []
+    tags: [],
   };
 
   const baseApiUrl = import.meta.VITE_BASE_API_URL;
-  const [posts, setPosts] = useState([])
-  const [formData, setFormData] = useState(initialFormData)
+  const [posts, setPosts] = useState([]);
+  const [formData, setFormData] = useState(initialFormData);
 
-  const fetchPosts = () => (
-    axios.get(`${baseApiUrl}/posts`)
-    .then(res => (
-      setPosts(res.data)
-    ))
-  )
+  const fetchPosts = () =>
+    axios.get(`${baseApiUrl}/posts`).then((res) => setPosts(res.data));
+
   return (
     <>
+      <div className="container">
+        <h1>Elenco Post</h1>
+        <div className="row"></div>
+      </div>
     </>
   );
 }
